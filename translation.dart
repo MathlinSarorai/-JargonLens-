61d53f2f4225a8bb26c47cf96c80455fe9d694a1
Mathlin sarorai A S <116174432+MathlinSarorai@users.noreply.github.com>
2024-04-11 12:18:51 +0530

    translation.dart

--- translation.dart
+++ translation.dart
@@ -1,100 +1,57 @@
-import 'dart:io';
-
+import 'package:dating_app/translator.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:google_ml_kit/google_ml_kit.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:translator/translator.dart';
 
-class Translation extends StatelessWidget {
-  const Translation({super.key});
+class Translation extends StatefulWidget {
+  final String regText;
+  Translation({required this.regText});
+  @override
+  State<Translation> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<Translation> {
+  @override
+  final translator = GoogleTranslator();
+
+  String _translatedText = '';
+
+  void translateText() {
+    translator.translate(widget.regText, to: 'ta').then((value) {
+      setState(() {
+        _translatedText = value.text;
+      });
+    }).catchError((error) {
+      print("Translation Error: $error");
+    });
+  }
 
   @override
+  void initState() {
+    super.initState();
+    // Call translateText function when the page is initialized
+    translateText();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Translation Log',
-          style: TextStyle(
-            color: Colors.white,
-          ),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          title: const Text('English to Tamil Translator'),
+          leading: IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyHomePage()),
+                );
+              },
+              icon: const Icon(Icons.arrow_back)),
         ),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            'Translate Content',
-            textAlign: TextAlign.center,
-            style: GoogleFonts.lato(fontSize: 30, color: Colors.black),
+        body: Center(
+          child: Text(
+            _translatedText,
+            style: const TextStyle(fontSize: 18.0),
           ),
-          Container(
-              margin: const EdgeInsets.symmetric(horizontal: 5),
-              padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  shadowColor: Colors.grey[400],
-                  elevation: 10,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0)),
-                ),
-                onPressed: () {},
-                child: Container(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                  child: const Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(
-                        Icons.translate,
-                        size: 30,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        "English",
-                        style: TextStyle(fontSize: 13, color: Colors.white),
-                      )
-                    ],
-                  ),
-                ),
-              )),
-          Icon(
-            Icons.arrow_downward_sharp,
-            weight: 500,
-            size: 50,
-          ),
-          Container(
-              margin: const EdgeInsets.symmetric(horizontal: 5),
-              padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  shadowColor: Colors.grey[400],
-                  elevation: 10,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0)),
-                ),
-                onPressed: () {},
-                child: Container(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                  child: const Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(Icons.translate, size: 30, color: Colors.white),
-                      Text(
-                        "Language",
-                        style: TextStyle(fontSize: 13, color: Colors.white),
-                      )
-                    ],
-                  ),
-                ),
-              )),
-        ],
-      ),
-    );
+        ));
   }
 }
